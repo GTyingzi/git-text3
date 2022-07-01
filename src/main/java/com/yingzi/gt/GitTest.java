a1db3cac8b6169fb17a67f588324f39c343eb6ab
yingzi <guotaomath@163.com>
2022-07-01 22:53:09 +0800

    second commit

--- src/main/java/com/yingzi/gt/GitTest.java
+++ src/main/java/com/yingzi/gt/GitTest.java
@@ -3,5 +3,6 @@ package com.yingzi.gt;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello gitl");
+        System.out.println("hello gitl2");
     }
 }
