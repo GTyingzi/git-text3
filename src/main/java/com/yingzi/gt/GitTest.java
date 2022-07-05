f66679127d4e1afb5edfbd40943ad81eb6ab9fcd
yingzi <guotaomath@163.com>
2022-07-05 12:16:45 +0800

    push commit

--- src/main/java/com/yingzi/gt/GitTest.java
+++ src/main/java/com/yingzi/gt/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello gitl2");
         System.out.println("hello gitl3");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
